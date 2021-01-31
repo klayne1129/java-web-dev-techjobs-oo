54faf3373ab8e63f847e73d91c75fb12e25bde9a
Katie Layne <klayne1129@gmail.com>
2021-01-31 17:40:24 -0600

    Updated toString method.

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -26,9 +26,6 @@ public class Job {
     public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
         this();
         this.name = name;
-        if (this.name == null || "".equals(this.name)) {
-            this.name="not available";
-        }
         this.employer = employer;
         this.location = location;
         this.positionType = positionType;
@@ -39,85 +36,108 @@ public class Job {
 
     @Override
     public String toString() {
-        if (this.name ==null && this.employer ==null && this.location ==null && this.positionType ==null && this.coreCompetency ==null ) {
+            if (this.name == null && this.employer == null && this.location == null && this.positionType == null && this.coreCompetency == null) {
 
             return "\n" + "OOPS! This job, ID: " + id + " does not seem to exist." + "\n";
+            }
+            if (name.equals("")) {
+            name = "Data not available";
+            }
 
-        }else{
+            if (employer.getValue().equals("")) {
+                    employer.setValue("Data not available");
+
+            }
+
+            if (location.getValue().equals("")) {
+                    location.setValue("Data not available");
+            }
+
+            if (positionType.getValue().equals("")) {
+                    positionType.setValue("Data not available");
+            }
+
+            if (coreCompetency.getValue().equals("")) {
+            coreCompetency.setValue("Data not available");
+
+            }
+
             return "\n" +
-                    "ID: " + id + "\n" +
-                    "Name: " + name + "\n" +
-                    "Employer: " + employer + "\n" +
-                    "Location: " + location + "\n" +
-                    "Position Type: " + positionType + "\n" +
-                    "Core Competency: " + coreCompetency + "\n";
+                            "ID: " + id + "\n" +
+                            "Name: " + name + "\n" +
+                            "Employer: " + employer + "\n" +
+                            "Location: " + location + "\n" +
+                            "Position Type: " + positionType + "\n" +
+                            "Core Competency: " + coreCompetency + "\n";
         }
 
-    }
 
 
-    // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
-    //  match.
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Job)) return false;
-        Job job = (Job) o;
-        return getId() == job.getId();
-    }
+            // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
+            //  match.
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Job job = (Job) o;
+                return id == job.id;
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(id);
+            }
 
 
-    // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
-    //  and id.
+            // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
+            //  and id.
 
 
     public int getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public String getName () {
+            return name;
+        }
+
+        public void setName (String name){
+            this.name = name;
+        }
+
+        public Employer getEmployer () {
+            return employer;
+        }
+
+        public void setEmployer (Employer employer){
+            this.employer = employer;
+        }
+
+        public Location getLocation () {
+            return location;
+        }
+
+        public void setLocation (Location location){
+            this.location = location;
+        }
+
+        public PositionType getPositionType () {
+            return positionType;
+        }
+
+        public void setPositionType (PositionType positionType){
+            this.positionType = positionType;
+        }
+
+        public CoreCompetency getCoreCompetency () {
+            return coreCompetency;
+        }
+
+        public void setCoreCompetency (CoreCompetency coreCompetency){
+            this.coreCompetency = coreCompetency;
+        }
+
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Employer getEmployer() {
-        return employer;
-    }
-
-    public void setEmployer(Employer employer) {
-        this.employer = employer;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setLocation(Location location) {
-        this.location = location;
-    }
-
-    public PositionType getPositionType() {
-        return positionType;
-    }
-
-    public void setPositionType(PositionType positionType) {
-        this.positionType = positionType;
-    }
-
-    public CoreCompetency getCoreCompetency() {
-        return coreCompetency;
-    }
-
-    public void setCoreCompetency(CoreCompetency coreCompetency) {
-        this.coreCompetency = coreCompetency;
-    }
-}
